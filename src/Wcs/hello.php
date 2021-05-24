bd8f7f659bf784d4e437c8b66d3a4893e81d3c9f
Wowlfy <maxime.giraudeau@hotmail.fr>
2021-05-24 17:51:21 +0200

    first composer

---
+++ src/Wcs/hello.php
@@ -0,0 +1,10 @@
+<?php
+
+class Hello
+{
+
+    public function talk()
+    {
+        echo "hello world !";
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0afb0bf907df15587d7d89065ac8fb5e92a39c0c',
+    'name' => 'wowlfyy/composer',
+  ),
+  'versions' => 
+  array (
+    'wowlfyy/composer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0afb0bf907df15587d7d89065ac8fb5e92a39c0c',
+    ),
+  ),
+);
